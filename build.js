d60714cee0d211b3120d38ef3c2d757f00dde1a6
willyb321 <wbwilliam7@gmail.com>
2017-09-02 09:54:03 +1000

    move pug to views/,  add some css, file watch, editorconfig,
    fixed boopsuite desc

--- build.js
+++ build.js
@@ -1,7 +1,8 @@
 const pug = require('pug');
 const fs = require('fs');
+
 // Compile the template.
-const compiledFunction = pug.compileFile('index.pug');
+const compiledFunction = pug.compileFile('views/index.pug');
 
 // Read tools listing.
 const tools = JSON.parse(fs.readFileSync('./_data/tools.json', "utf8"));
