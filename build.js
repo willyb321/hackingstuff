85b3f0813d959ee9e104d994d474bb2ebc0073c0
willyb321 <wbwilliam7@gmail.com>
2017-09-01 15:08:59 +1000

    add data file to generate site from, add favicon, layout and more

--- build.js
+++ build.js
@@ -1,6 +1,10 @@
 const pug = require('pug');
 const fs = require('fs');
-// Compile the source code
+// Compile the template.
 const compiledFunction = pug.compileFile('index.pug');
 
-fs.writeFileSync('index.html', compiledFunction());
+// Read tools listing.
+const tools = JSON.parse(fs.readFileSync('./_data/tools.json', "utf8"));
+
+// Write index.html
+fs.writeFileSync('./dist/index.html', compiledFunction({tools: tools}));
